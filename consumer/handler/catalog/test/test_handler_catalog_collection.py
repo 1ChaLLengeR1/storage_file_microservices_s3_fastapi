d332b6ee43a84c5c584c16459b5796a16b15bc93
1ChaLLengeR1 <92648464+1ChaLLengeR1@users.noreply.github.com>
2024-10-20 21:36:21 +0200

    - added test for handler collection one - added test for api collection one

--- consumer/handler/catalog/test/test_handler_catalog_collection.py
+++ consumer/handler/catalog/test/test_handler_catalog_collection.py
@@ -3,6 +3,7 @@ from config.celery_config import app
 from decouple import config
 from consumer.handler.catalog.create import handler_create_catalog
 from consumer.handler.catalog.collection import handler_collection_catalog
+from consumer.handler.catalog.collection_one import handler_collection_one_catalog
 from consumer.helper.random import createRandom
 from consumer.data.error import ResponseError
 
@@ -28,3 +29,23 @@ def test_handler_collection_catalog():
     print("Test handler collection catalog passed!")
 
 
+def test_handler_collection_one_catalog():
+    catalog_name = createRandom("test")
+    bucket_name = config("AWS_BUCKET_NAME")
+    key_create = "test"
+    response_create = handler_create_catalog(bucket_name, catalog_name, key_create)
+
+    key_main = "test1"
+    response_collection_one = handler_collection_one_catalog(response_create.id, key_main)
+
+    if isinstance(response_collection_one, ResponseError):
+        pytest.fail(f"Error from test handler collection one catalog: {response_collection_one}")
+
+    assert isinstance(response_collection_one, dict), "Expected collection one to be a dictionary."
+
+    assert response_create.name == response_collection_one['name'], (
+        f"Expected name from response_create to match name from response_collection_one, "
+        f"but got {response_create.name} and {response_collection_one['name']}"
+    )
+
+    print("Test handler collection one catalog passed!")
